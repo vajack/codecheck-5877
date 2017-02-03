6ee66113fb15bc72c07a12fd12017acd7a769f36
Yuki-Ikegami <fp25657.hawks@icloud.com>
2017-02-03 16:39:33 +0900

    ai complete

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -2,6 +2,12 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-        
+        for (int i = 1, l = args.length; i < l; i++) {
+            if(args[0].charAt(args[0].length()-1) == args[i].charAt(0)){
+                System.out.println(args[i]);
+                return;
+            }
+        }
+        System.out.println("string not found");
 	}
 }
